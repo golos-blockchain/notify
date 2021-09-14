54fe405cba7cdfba6418cb988e4f502bee5bef61
1aerostorm <aerostorm1@gmail.com>
2021-09-14 22:29:01 +0300

    Remove ALLOWED_CLIENTS, add AUTH_HOST and SITE_DOMAIN

--- dataserver/run-dev.sh
+++ dataserver/run-dev.sh
@@ -4,7 +4,7 @@ export TARANTOOL_HOST=127.0.0.1
 export NODE_URL=ws://127.0.0.1:8091
 export CHAIN_ID=5876894a41e6361bde2e73278f07340f2eb8b41c2facd29099de9deef6cdb679
 export SESSION_SECRET=should-be-really-generated-secret
-export ALLOWED_CLIENTS="localhost 127.0.0.1"
-export AUTH_HOST=127.0.0.1:8080
+export AUTH_HOST=http://127.0.0.1:8080
+export SITE_DOMAIN=127.0.0.1
 
 nodemon
