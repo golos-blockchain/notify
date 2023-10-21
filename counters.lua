3b9627e91123df7302db4082e25cf9301190d80f
1aerostorm <aerostorm1@gmail.com>
2023-10-21 18:06:05 +0000

    Counters API - Add NFT counters, and extra 10 counters

--- counters.lua
+++ counters.lua
@@ -1,14 +1,25 @@
 require 'table_utils'
 
+row_len = 26
+
+function migrate_tuple(space, account, cur_len)
+    if cur_len < row_len then
+        for i=cur_len+1,row_len do
+            space:update(account, {{'!', i, 0}})
+        end
+    end
+end
+
 function counter_add(account, scope)
     -- print('counter_add -->', account, scope)
     local space = box.space.counters
     local res = space:select{account}
     if #res > 0 then
         local tuple = res[1]
+        migrate_tuple(space, account, #tuple)
         space:update(account, {{'+', 2, 1}, {'+', scope + 2, 1}})
     else
-        local tuple = {account, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+        local tuple = {account, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
         tuple[scope + 2] = 1;
         space:insert(tuple)
     end
@@ -22,6 +33,7 @@ function counter_read(account, scope)
   local tuple = res[1]
   local count = tuple[scope + 2]
   if count == nil or count <= 0 then return tuple end
+  migrate_tuple(space, account, #tuple)
   local res = space:update(account, {{'-', 2, count}, {'=', scope + 2, 0}})
   return res
 end
