24a1e1a82e2155b4e6ae960e99a3a8cb5bfe3124
1aerostorm <aerostorm1@gmail.com>
2024-09-13 05:46:03 +0300

    (HF 30) - send_offchain groups

--- dataserver/api/msgs.js
+++ dataserver/api/msgs.js
@@ -5,12 +5,13 @@ const { returnError, SCOPES } = require('../utils');
 const { GOLOS_CHECK_VALUE, GOLOS_CHECK_PARAM_ACCOUNT } = require('../offchain_validators');
 const { signal_fire } = require('../signals');
 const { putToQueues } = require('./queues');
+const { putToGroupQueues } = require('./group_queues')
 const isBlocking = require('../isBlocking')
 
 class private_message_operation {
     constructor(obj) {
         const { from, to, nonce, from_memo_key, to_memo_key,
-            checksum, update, encrypted_message, } = obj;
+            checksum, update, encrypted_message, extensions, } = obj;
         this.from = from;
         this.to = to;
         this.nonce = nonce;
@@ -19,18 +20,36 @@ class private_message_operation {
         this.checksum = checksum;
         this.update = update;
         this.encrypted_message = encrypted_message;
+
+        if (extensions) {
+            for (const ext of extensions) {
+                const [ key, data ] = ext
+                if (key === 0) {
+                    this.has_group = true
+                    this.group = data.group
+                    this.requester = data.requester || ''
+                    this.mentions = data.mentions || []
+                }
+            }
+        }
     }
 
     getAuthority() {
+        if (this.update) {
+            if (this.requester) return this.requester
+        }
         return this.from;
     }
 
     validate() {
         const { from, to, nonce, from_memo_key, to_memo_key,
-            checksum, update, encrypted_message, } = this;
+            checksum, update, encrypted_message,
+            group, requester, mentions } = this
 
         GOLOS_CHECK_PARAM_ACCOUNT(this, 'from');
-        GOLOS_CHECK_PARAM_ACCOUNT(this, 'to');
+        if (!group || to) {
+            GOLOS_CHECK_PARAM_ACCOUNT(this, 'to');
+        }
 
         GOLOS_CHECK_VALUE(from != to, 'You cannot write to yourself');
 
@@ -47,6 +66,18 @@ class private_message_operation {
 
         GOLOS_CHECK_VALUE(typeof(encrypted_message) === 'string' && encrypted_message.length >= 16,
             'Encrypted message is too small');
+
+        if (this.has_group) {
+            GOLOS_CHECK_VALUE(!!group, 'Invalid group')
+        }
+        if (requester) {
+            GOLOS_CHECK_PARAM_ACCOUNT(this, 'requester')
+        }
+        if (mentions) {
+            for (let i = 0; i < mentions.length; ++i) {
+                GOLOS_CHECK_PARAM_ACCOUNT(mentions, i)
+            }
+        }
     }
 };
 
@@ -72,35 +103,90 @@ module.exports = function useMsgsApi(app) {
 
             op.validate();
 
-            const accs = await golos.api.getAccountsAsync([from, to]);
+            const names = [from]
+            if (to) names.push(to)
+            const accs = await golos.api.getAccountsAsync(names);
             GOLOS_CHECK_VALUE(accs.length !== 0 && accs[0].name === from,
                 'Missing account from');
-            GOLOS_CHECK_VALUE(accs.length === 2,
-                'Missing account to');
+            if (to) {
+                GOLOS_CHECK_VALUE(accs.length === 2,
+                    'Missing account to');
+            }
 
             fromAcc = accs[0]
             toAcc = accs[1]
 
-            GOLOS_CHECK_VALUE(from_memo_key === fromAcc.memo_key,
-                'from_memo_key is not match with from account memo_key');
+            if (op.group) {
+                const { emptyPublicKey } = golos.messages
+                GOLOS_CHECK_VALUE(from_memo_key === emptyPublicKey,
+                    'Memo keys should be empty for groups')
+                GOLOS_CHECK_VALUE(to_memo_key === emptyPublicKey,
+                    'Memo keys should be empty for groups')
 
-            GOLOS_CHECK_VALUE(to_memo_key === toAcc.memo_key,
-                'to_memo_key is not match with to account memo_key');
+                const reqMems = []
+                const auth = op.getAuthority()
+                if (auth !== from) {
+                    reqMems.push(auth)
+                }
+                let pgo = await golos.api.getGroupsAsync({
+                    start_group: op.group,
+                    limit: 1,
+                    with_members: {
+                        accounts: reqMems
+                    },
+                })
+                GOLOS_CHECK_VALUE(pgo[0] && pgo[0].name === op.group,
+                    'Missing group')
+                if (auth !== from) {
+                    console.log(pgo)
+                }
+            } else {
+                GOLOS_CHECK_VALUE(from_memo_key === fromAcc.memo_key,
+                    'from_memo_key is not match with from account memo_key');
+
+                if (toAcc)
+                    GOLOS_CHECK_VALUE(to_memo_key === toAcc.memo_key,
+                        'to_memo_key is not match with to account memo_key');
+            }
         } catch (error) {
             return returnError(ctx, error.message);
         }
 
         const now = new Date().toISOString().split('.')[0];
 
-        const blocking = await isBlocking(toAcc, fromAcc)
-        if (blocking) {
-            if (blocking === 1) {
-                console.error(`/msgs/send_offchain @${from} wants to bypass blacklist of @${to}`)
-            } else {
-                console.error(`/msgs/send_offchain @${from} wants to bypass do-not-bother preset of @${to}`)
+        if (toAcc) {
+            const blocking = await isBlocking(toAcc, fromAcc)
+            if (blocking) {
+                if (blocking === 1) {
+                    console.error(`/msgs/send_offchain @${from} wants to bypass blacklist of @${to}`)
+                } else {
+                    console.error(`/msgs/send_offchain @${from} wants to bypass do-not-bother preset of @${to}`)
+                }
+                ctx.body = {
+                    status: 'ok',
+                }
+                return
             }
-            ctx.body = {
-                status: 'ok',
+        }
+
+        if (op.group) {
+            try {
+                console.log(op.group)
+                await putToGroupQueues(
+                    op.group,
+                    'message',
+                    ['private_message', {...params, _offchain: true}],
+                    now
+                )
+                ctx.body = {
+                    status: 'ok',
+                }
+            } catch (error) {
+                console.error(`[reqid ${ctx.request.header['x-request-id']}] ${ctx.method} ERRORLOG /msgs/send_offchain /${group} ${error.message}`)
+                ctx.body = {
+                    status: 'err',
+                    error: 'Tarantool error when notifying group watchers',
+                }
             }
             return
         }
@@ -136,6 +222,10 @@ module.exports = function useMsgsApi(app) {
                 error: 'Tarantool error when notifying to',
             };
             return;
+        }
+
+        ctx.body = {
+            status: 'ok',
         }
     });
 
