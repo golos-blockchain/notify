743abee506365f8a0aaa921c132ebc2e9e1f06bc
1aerostorm <78693755+1aerostorm@users.noreply.github.com>
2024-12-14 17:21:41 +0300

    Update feed.js

--- dataserver/feed.js
+++ dataserver/feed.js
@@ -18,7 +18,7 @@ function getPostKey(op) {
 }
 
 async function cleanupQueues() {
-    console.log('cleanupQueues (just update it 2)');
+    console.log('cleanupQueues (just update it 3)');
     const res = await Tarantool.instance('tarantool').call(
         'queue_list_for_cleanup');
     if (!res[0][0]) return;
